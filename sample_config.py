fb264045c118afb3f98ecaaf6ce071ed656e875f
ashutosh008-cmd <83965991+ashutosh008-cmd@users.noreply.github.com>
2023-06-06 18:24:23 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,23 +2,23 @@ from os import getenv
 
 from decouple import config
 
-APP_ID = getenv("APP_ID", "16942102")
+APP_ID = getenv("APP_ID", "1945402")
 
-API_HASH = getenv("API_HASH", "eb72f958fd9dbad872a9ff1836ad96e6")
+API_HASH = getenv("API_HASH", "5fe8b55719270b8163bff9d32bdaaa78")
 
 HEROKU_APP_NAME = config("HEROKU_APP_NAME", None)
 HEROKU_API_KEY = config("HEROKU_API_KEY", None)
 
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-BOT_TOKEN2 = config("BOT_TOKEN2", default=None)
-BOT_TOKEN3 = config("BOT_TOKEN3", default=None)
-BOT_TOKEN4 = config("BOT_TOKEN4", default=None)
-BOT_TOKEN5 = config("BOT_TOKEN5", default=None)
-BOT_TOKEN6 = config("BOT_TOKEN6", default=None)
-BOT_TOKEN7 = config("BOT_TOKEN7", default=None)
-BOT_TOKEN8 = config("BOT_TOKEN8", default=None)
-BOT_TOKEN9 = config("BOT_TOKEN9", default=None)
-BOT_TOKEN10 = config("BOT_TOKEN10", default=None)
+BOT_TOKEN = config("BOT_TOKEN", default=5765591009:AAH_3EezVnAq89Y0dAMbRNwL1uzBobMsCUk)
+BOT_TOKEN2 = config("BOT_TOKEN2", default=6251600257:AAHwaoPzL23X7Exb2l6-P_WaD8bKDL50pL4)
+BOT_TOKEN3 = config("BOT_TOKEN3", default=5886318269:AAECwLbBMMuqe7j_zj87kQqnYL94e_S4ZjY)
+BOT_TOKEN4 = config("BOT_TOKEN4", default=5677671821:AAGL8uL_YZLShXpBZ9DXdtoVUTBXRlQhQoM)
+BOT_TOKEN5 = config("BOT_TOKEN5", default=5678493977:AAGPWTlTqtKRIl1PKG9IovrCtUqI3VFv1Ak)
+BOT_TOKEN6 = config("BOT_TOKEN6", default=5470518560:AAF3X0WKSS2bM3eAFVNtrYeJRe7bYNKhbOY)
+BOT_TOKEN7 = config("BOT_TOKEN7", default=6153225183:AAGtCy87cph9JMbfr8tXhQHx_Gr4Sybg_fc)
+BOT_TOKEN8 = config("BOT_TOKEN8", default=6054850344:AAGPbY388cKhfUxkvj869pEbNU4hECkVgKU)
+BOT_TOKEN9 = config("BOT_TOKEN9", default=6152781098:AAHS9orsvvEqRYoEWYp4tNacq01ZtS_pChc)
+BOT_TOKEN10 = config("BOT_TOKEN10", default=5937423293:AAGkWjaGaqB3tB-V7_LnI8Fx_6ngpGHvCWQ)
 BOT_TOKEN11 = config("BOT_TOKEN11", default=None)
 BOT_TOKEN12 = config("BOT_TOKEN12", default=None)
 BOT_TOKEN13 = config("BOT_TOKEN13", default=None)
@@ -35,9 +35,9 @@ BOT_TOKEN23 = config("BOT_TOKEN23", default=None)
 BOT_TOKEN24 = config("BOT_TOKEN24", default=None)
 BOT_TOKEN25 = config("BOT_TOKEN25", default=None)
 try:
-    SUDO_USERS = str(getenv("SUDO_USERS", "123 456")).split(" ")
+    SUDO_USERS = str(getenv("SUDO_USERS", "5148516372")).split(" ")
 except Exception:
-    SUDO_USERS = str(getenv("SUDO_USERS", "123 456"))
+    SUDO_USERS = str(getenv("SUDO_USERS", "5003311088"))
 
 START_MESSAGE = getenv("START_MESSAGE", None)
 
@@ -47,7 +47,7 @@ START_PIC = getenv("START_PIC", None)
 
 
 HELP_MSG = getenv("HELP_MSG", None)
-HELP_PIC = getenv("HELP_PIC", "https://graph.org/file/89ed7d3a2bd8aa2c61385.jpg")
+HELP_PIC = getenv("HELP_PIC", "https://graph.org/file/6b8349c4e936e0314d605.jpg")
 LOG_CHANNEL = getenv("LOG_CHANNEL", None)
 
 HANDLER = getenv("HANDLER", "/")
